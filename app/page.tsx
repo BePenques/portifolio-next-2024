b08c3bccff452c87216edc053aafe747264976e9
Betiza Barreira <be.penques@hotmail.com>
2024-11-20 17:26:12 -0300

    seção conhecimentos

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { HeroSection } from './components/pages/home/hero-section'
+import { KnowTechs } from './components/pages/know-techs/index'
 
 export default async function Home() {
   return (
     <>
       <HeroSection />
+      <KnowTechs />
     </>
   )
 }
